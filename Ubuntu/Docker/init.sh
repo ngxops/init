19c5402d43277d015239abb04dfff3ef8c4b456a
ngxops <ngx1990@gmail.com>
2021-08-13 16:23:14 +0800

    update

--- Ubuntu/Docker/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- # 安装基本组件
-apt-get update
-apt autoremove -y
-apt-get upgrade -y
-sed -i 's/enforcing/disabled/g' /etc/selinux/config
-apt-get install -y vim openssh* ntp wget screen bash-completion net-tools
-apt-get -y update libssh*
-apt install -y xfsprogs iptables-persistent
--- Ubuntu/Docker/oneinstack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 初始化安装oneinstack
-cd /usr/local/src/
-wget http://mirrors.linuxeye.com/oneinstack-full.tar.gz
-tar zxvvf oneinstack-full.tar.gz
-
-cd /usr/local/src/ && ./oneinstack/install.sh --reboot
